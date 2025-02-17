5fd97b668d2d38fc9000ceca3d5bd7ad8e834645
Aditya Patnaik <aditya.narayan.patnaik@gmail.com>
2025-02-18 02:14:04 +0530

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  host: 'mydomain.local',
   preview: {
     host: '0.0.0.0',  // Allow access from all interfaces
     port: 4173,  // Or any port you prefer
