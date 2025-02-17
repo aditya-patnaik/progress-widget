5e579bb61bdaf6015fb94838398d02f760fe2e35
Aditya Patnaik <aditya.narayan.patnaik@gmail.com>
2025-02-18 02:22:31 +0530

    adding base value to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/progress-widget/",
   preview: {
     host: '0.0.0.0',  // Allow access from all interfaces
     port: 4173,  // Or any port you prefer
